dbcedf9daa59ecb2bac9f1483258c94541805aea
QizhouWang <qizhouwang.nanjing@gmail.com>
2022-04-14 09:37:33 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from torch.autograd import Variable
 
 from wideresnet import *
 from resnet import *
-# from mart import mart_loss
 from at_loss import at_loss
 from mail_loss import *
 import numpy as np
@@ -55,9 +54,9 @@ parser.add_argument('--method', default='trades_mail',
 parser.add_argument('--save-freq', '-s', default=1, type=int, metavar='N',
                     help='save frequency')
 
-parser.add_argument('--bias', default=-1.5,
+parser.add_argument('--bias', type=float, default=-1.5,
                     help='weighting bias term')
-parser.add_argument('--slope', default=1.0,
+parser.add_argument('--slope', type=float, default=1.0,
                     help='weighting bias term')
 args = parser.parse_args()
 
@@ -82,14 +81,14 @@ testset = torchvision.datasets.CIFAR10(root='../data_attack/', train=False, down
 test_loader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size, shuffle=False, num_workers=10)
 
 
-def train(args, model, device, train_loader, optimizer, epoch, bias = None, slope = None):
+def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
                     
         # calculate robust loss
-        if 'mail' not in args.method:
+        if 'mail' not in args.method or epoch <= 75:
             loss = at_loss(model=model,
                             x_natural=data,
                             y=target,
@@ -100,13 +99,11 @@ def train(args, model, device, train_loader, optimizer, epoch, bias = None, slop
                             beta=args.beta,
                             method = args.method)
         else:
-            if bias is None or slope is None: 
-                bias, slope = args.bias, args.slope
             loss = mail_loss(model=model,
                             x_natural=data,
                             y=target,
                             optimizer=optimizer,
-                            bias = bias, slope = slope,
+                            bias = args.bias, slope = args.slope,
                             step_size=args.step_size,
                             epsilon=args.epsilon,
                             perturb_steps=args.num_steps,
@@ -123,28 +120,15 @@ def train(args, model, device, train_loader, optimizer, epoch, bias = None, slop
 
 def adjust_learning_rate(optimizer, epoch, model, method):
     """decrease the learning rate"""
-    if args.epochs == 120: 
-        lr = args.lr
-        if epoch >= 100:
-            lr = args.lr * 0.001
-        elif epoch >= 90:
-            lr = args.lr * 0.01
-        elif epoch >= 75:
-            lr = args.lr * 0.1
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-        if epoch == 70: torch.save(model.state_dict(), 'models/resnet75_90' + method + '.pth')
-    else: 
-        lr = args.lr
-        if epoch >= 90:
-            lr = args.lr * 0.001
-        elif epoch >= 60:
-            lr = args.lr * 0.01
-        elif epoch >= 30:
-            lr = args.lr * 0.1
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-        if epoch == 55: torch.save(model.state_dict(), 'models/resnet30_60' + method + '.pth')
+    lr = args.lr
+    if epoch >= 100:
+        lr = args.lr * 0.001
+    elif epoch >= 90:
+        lr = args.lr * 0.01
+    elif epoch >= 75:
+        lr = args.lr * 0.1
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
 def cwloss(output, target,confidence=50, num_classes=10):
     target = target.data
@@ -154,9 +138,10 @@ def cwloss(output, target,confidence=50, num_classes=10):
     target_var = Variable(target_onehot, requires_grad=False)
     real = (target_var * output).sum(1)
     other = ((1. - target_var) * output - target_var * 10000.).max(1)[0]
-    loss = -torch.clamp(real - other + confidence, min=0.)  # equiv to max(..., 0.)
+    loss = -torch.clamp(real - other + confidence, min=0.) 
     loss = torch.sum(loss)
     return loss
+
 def _pgd_whitebox(model,
                   X,
                   y,
@@ -198,7 +183,8 @@ def eval_adv_test_whitebox(model, device, test_loader):
         # pgd attack
         X, y = Variable(data, requires_grad=True), Variable(target)
         err_na, err_20 = _pgd_whitebox(model, X, y)
-        err_na, err_100 = _pgd_whitebox(model, X, y, num_steps = 100)
+        # err_na, err_100 = _pgd_whitebox(model, X, y, num_steps = 100)
+        err_100 = 0
         _, err_cw = _pgd_whitebox(model, X, y, loss_fn='cw')
         
         na_err_total += err_na
@@ -208,29 +194,16 @@ def eval_adv_test_whitebox(model, device, test_loader):
     
     print('na %.4f | 20 %.4f | 100 %.4f | cw %.4f ' % (1 - na_err_total / len(test_loader.dataset),1- ce20_err_total / len(test_loader.dataset),1- ce100_err_total / len(test_loader.dataset),1- cw_err_total / len(test_loader.dataset)))
 
-def main(bias = None, slope = None):
+def main():
     model = ResNet18().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)   
-    
-    begin_epoch = 0
-    if 'mail' in args.method:
-        if 'mart' in args.method:
-            model.load_state_dict(torch.load('models/resnet75_90mart.pth' if args.epochs == 120 else 'models/resnet30_60mart.pth'))
-        elif 'at' in args.method: 
-            model.load_state_dict(torch.load('models/resnet75_90at.pth' if args.epochs == 120 else 'models/resnet30_60at.pth'))
-        elif 'trades' in args.method: 
-            model.load_state_dict(torch.load('models/resnet75_90trades.pth' if args.epochs == 120 else 'models/resnet30_60trades.pth'))
-        else: 
-            raise RuntimeError('invalid method name')
-        begin_epoch = 75 if args.epochs == 120 else 60
-        
-    for epoch in range(begin_epoch, args.epochs):
+    for epoch in range(args.epochs):
         adjust_learning_rate(optimizer, epoch, model, args.method)
 
         start_time = time.time()
-        train(args, model, device, train_loader, optimizer, epoch, bias, slope)
+        train(args, model, device, train_loader, optimizer, epoch)
 
-        if epoch == 100:
+        if epoch % 10 == 9:
             print() 
             eval_adv_test_whitebox(model, device, test_loader)
             x_test = torch.cat([x for (x, y) in test_loader], 0)
@@ -238,7 +211,6 @@ def main(bias = None, slope = None):
             adversary = AutoAttack(model, norm='Linf', eps=0.031, version='standard')
             adv_complete = adversary.run_standard_evaluation(x_test[:1000], y_test[:1000], bs=500)
             print()
-            break
     
 
 if __name__ == '__main__':
